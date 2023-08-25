7a0af2705eb597d30cef024138c593600d1cdcdc
AissatouCamara <aissatoucra@gmail.com>
2023-08-25 12:53:45 -0700

    added tshirt model, repo, controller, and test

--- gamestore/src/main/java/com/company/gamestore/GamestoreApplication.java
+++ gamestore/src/main/java/com/company/gamestore/GamestoreApplication.java
@@ -2,8 +2,9 @@ package com.company.gamestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
 public class GamestoreApplication {
 
 	public static void main(String[] args) {
