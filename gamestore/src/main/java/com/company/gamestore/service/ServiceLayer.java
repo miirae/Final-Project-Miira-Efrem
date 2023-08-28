94aaabcec55b1c23133fcb6b47a807eb2e444479
Santiago F <santiferuseche@gmail.com>
2023-08-28 16:16:40 -0500

    add gamerepo to servicelayer constructor

--- gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
+++ gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
@@ -1,10 +1,7 @@
 package com.company.gamestore.service;
 
 import com.company.gamestore.model.Invoice;
-import com.company.gamestore.repository.FeeRepository;
-import com.company.gamestore.repository.InvoiceRepository;
-import com.company.gamestore.repository.TaxRepository;
-import com.company.gamestore.repository.TshirtRepository;
+import com.company.gamestore.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,23 +13,24 @@ public class ServiceLayer {
     private InvoiceRepository invoiceRepository;
     private TaxRepository taxRepository;
     private TshirtRepository tshirtRepository;
-    // private GameRepository gameRepository;
+    private GameRepository gameRepository;
     // private ConsoleRepository consoleRepository;
 
     // TODO: add these
-    // GameRepository gameRepository
     // ConsoleRepository consoleRepository
     @Autowired
     public ServiceLayer(
             FeeRepository feeRepository,
             InvoiceRepository invoiceRepository,
             TaxRepository taxRepository,
-            TshirtRepository tshirtRepository){
+            TshirtRepository tshirtRepository,
+            GameRepository gameRepository
+            ){
         this.feeRepository = feeRepository;
         this.invoiceRepository = invoiceRepository;
         this.taxRepository = taxRepository;
         this.tshirtRepository = tshirtRepository;
-        // this.gameRepository = gameRepository;
+        this.gameRepository = gameRepository;
         // this.consoleRepository = consoleRepository;
     }
 
