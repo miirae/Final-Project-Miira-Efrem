2f574133dc4d52707508fab0e0f43bd84b5b0227
Santiago F <santiferuseche@gmail.com>
2023-08-31 19:28:50 -0500

    finished invoice controller -> needs testing

--- gamestore/src/main/java/com/company/gamestore/controller/InvoiceController.java
+++ gamestore/src/main/java/com/company/gamestore/controller/InvoiceController.java
@@ -1,33 +1,47 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Invoice;
 import com.company.gamestore.service.ServiceLayer;
 import com.company.gamestore.viewmodel.InvoiceViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class InvoiceController {
+
+    // create invoice from vm
     @Autowired
     ServiceLayer invoiceServiceLayer;
-
-    //TODO: create invoice
     @PostMapping(path = "/invoice")
     @ResponseStatus(HttpStatus.CREATED)
-    public InvoiceViewModel createInvoice(@RequestBody @Valid InvoiceViewModel invoiceViewModel) {
-        //save into InvoiceViewModel
-        InvoiceViewModel i = new InvoiceViewModel();
-        return i;
+    public Invoice createInvoicefromVM(@RequestBody @Valid InvoiceViewModel ivm) {
+        return invoiceServiceLayer.saveInvoice(ivm);
     }
 
-    // TODO: get invoice by invoice id
-    // TODO: get all invoices
-    // TODO: find/get invoice by customer name
+    // get invoice by invoice id
+    @GetMapping(path = "/invoice/{InvoiceId}")
+    @ResponseStatus(HttpStatus.OK)
+    public Invoice getInvoiceByInvoiceId(@PathVariable int InvoiceId){
+        return invoiceServiceLayer.findById(InvoiceId);
+    }
+
+    // get all invoices
+    @GetMapping(path = "/invoice")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Invoice> getAllInvoices(){
+        return invoiceServiceLayer.findAll();
+    }
+
+    // find/get invoice by name
+    @GetMapping(path = "/invoice/name/{name}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Invoice> getInvoiceByName(@PathVariable String name){
+        return invoiceServiceLayer.findByName(name);
+    }
 
 
 
